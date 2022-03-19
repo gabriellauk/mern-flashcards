abb22dd136c1f067e30eedf593fb2829c1bc33e0
pisanzapra <pisanzapra@users.noreply.github.com>
2022-03-19 21:13:31 +0000

    Added React routes for Card List and Add Card

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 import CreateCard from "./components/CreateCard";
 import AllCards from "./components/AllCards";
 
 const App = () => {
-  return <div><CreateCard /><AllCards /></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/add" element={<CreateCard />} />
+        <Route path="/" element={<AllCards />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
