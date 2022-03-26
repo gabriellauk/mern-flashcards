600bef14582fc8f2ac1814ff6ed0f125a0e59b3b
pisanzapra <pisanzapra@users.noreply.github.com>
2022-03-26 19:52:11 +0000

    Fixed user model import issue

--- server/middlewares/authJwt.js
+++ server/middlewares/authJwt.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
-const db = require("../models");
-const User = db.user;
+const User = require("../models/user");
 
 verifyToken = (req, res, next) => {
   let token = req.headers["x-access-token"];
