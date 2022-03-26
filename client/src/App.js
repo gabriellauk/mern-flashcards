2ad9ce13ad32c728be977cd31e856b9f397fe074
pisanzapra <pisanzapra@users.noreply.github.com>
2022-03-26 19:50:28 +0000

    Added frontend functionality for login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,101 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AuthService from "./services/auth.service";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BoardUser from "./components/BoardUser";
 
 import CreateCard from "./components/CreateCard";
 import AllCards from "./components/AllCards";
 import EditCard from "./components/EditCard";
 import ViewCard from "./components/ViewCard.js";
 
-
 const App = () => {
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
+  const logOut = () => {
+    AuthService.logout();
+  };
   return (
     <div>
-      <Routes>
-        <Route path="/add" element={<CreateCard />} />
-        <Route path="/" element={<AllCards />} />
-        <Route path="/edit/:id" element={<EditCard />} />
-        <Route path="/card/:id" element={<ViewCard />} />
-      </Routes>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <Link to={"/"} className="navbar-brand">
+          Flashcards
+        </Link>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/"} className="nav-link">
+              Home
+            </Link>
+          </li>
+          {currentUser && (
+            <li className="nav-item">
+              <Link to={"/user"} className="nav-link">
+                User
+              </Link>
+            </li>
+          )}
+        </div>
+        {currentUser ? (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                {currentUser.username}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a href="/login" className="nav-link" onClick={logOut}>
+                Log out
+              </a>
+            </li>
+          </div>
+        ) : (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Log in
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Sign up
+              </Link>
+            </li>
+          </div>
+        )}
+      </nav>
+      <div className="container mt-3">
+        <Routes>
+          <Route exact path={"/"} element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route path="/user" element={<BoardUser />} />
+        </Routes>
+      </div>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <Routes>
+  //       <Route path="/add" element={<CreateCard />} />
+  //       <Route path="/" element={<AllCards />} />
+  //       <Route path="/edit/:id" element={<EditCard />} />
+  //       <Route path="/card/:id" element={<ViewCard />} />
+  //     </Routes>
+  //   </div>
+  // );
 };
 
 export default App;
---
+++ client/src/services/user.service.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+const API_URL = "http://localhost:5000/api/test/";
+const getPublicContent = () => {
+  return axios.get(API_URL + "all");
+};
+const getUserBoard = () => {
+  return axios.get(API_URL + "user", { headers: authHeader() });
+};
+export default {
+  getPublicContent,
+  getUserBoard,
+};
