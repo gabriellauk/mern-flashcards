198dbf60ee3ad70ce67ce42c70850377c250d163
pisanzapra <pisanzapra@users.noreply.github.com>
2022-05-21 15:39:53 +0100

    Security enhancement - Login now returns 401 regardless of specific issue

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -24,17 +24,17 @@ exports.signin = (req, res) => {
     username: req.body.username,
   }).exec((err, user) => {
     if (err) {
-      res.status(500).send({ message: err });
+      res.status(401).send({ message: err });
       return;
     }
     if (!user) {
-      return res.status(404).send({ message: "User Not found." });
+      return res.status(401).send({ message: "Invalid username or password" });
     }
     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordIsValid) {
       return res.status(401).send({
         accessToken: null,
-        message: "Invalid Password!",
+        message: "Invalid username or password!",
       });
     }
     var token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
