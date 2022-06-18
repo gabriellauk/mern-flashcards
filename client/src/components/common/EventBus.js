b04115aae7b24e7249cb1c422474b2ab6a13f0ee
pisanzapra <pisanzapra@users.noreply.github.com>
2022-06-18 13:04:15 +0100

    Refactored/cleaned up EventBus for consistency

--- client/src/components/common/EventBus.js
+++ client/src/components/common/EventBus.js
@@ -1,12 +1,19 @@
+const on = (event, callback) => {
+  document.addEventListener(event, (e) => callback(e.detail));
+};
+
+const dispatch = (event, data) => {
+  document.dispatchEvent(new CustomEvent(event, { detail: data }));
+};
+
+const remove = (event, callback) => {
+  document.removeEventListener(event, callback);
+};
+
 const eventBus = {
-    on(event, callback) {
-      document.addEventListener(event, (e) => callback(e.detail));
-    },
-    dispatch(event, data) {
-      document.dispatchEvent(new CustomEvent(event, { detail: data }));
-    },
-    remove(event, callback) {
-      document.removeEventListener(event, callback);
-    },
-  };
-  export default eventBus;
+  on,
+  dispatch,
+  remove,
+};
+
+export default eventBus;
