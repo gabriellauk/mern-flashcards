0acb26637eb1f674a7626d6989c907086294d04a
pisanzapra <pisanzapra@users.noreply.github.com>
2022-06-20 23:23:54 +0100

    Added comments

--- server/controllers/card.controller.js
+++ server/controllers/card.controller.js
@@ -1,17 +1,20 @@
 const Card = require("../models/card");
 
+// Fetch all active cards matching the user ID
 exports.activeCards = async (req, res) => {
   const query = { user: req.userId, active: true };
   const cards = await Card.find(query);
   res.status(200).json(cards);
 };
 
+// Fetch all inactive cards matching the user ID
 exports.inactiveCards = async (req, res) => {
   const query = { user: req.userId, active: false };
   const cards = await Card.find(query);
   res.status(200).json(cards);
 };
 
+// Save a new card
 exports.addCard = async (req, res) => {
   const newCard = new Card({
     frontText: req.body.frontText,
@@ -25,25 +28,35 @@ exports.addCard = async (req, res) => {
 };
 
 exports.deleteCard = async (req, res) => {
+  // Fetch the specific card by ID
   const card = await Card.findById(req.params.id);
+  // If the card's user ID does not match the user ID requested
   if (!card.user.equals(req.userId)) {
+    // Return forbidden
     return res.sendStatus(403);
   }
+  // Else, remove the card
   await card.remove();
   res.sendStatus(200);
 };
 
 exports.getSpecificCard = async (req, res) => {
+  // Fetch the specific card by ID
   const card = await Card.findById(req.params.id);
+  // If the card's user ID does not match the user ID requested
   if (!card.user.equals(req.userId)) {
+    // Return forbidden
     return res.sendStatus(403);
   }
   res.status(200).json(card);
 };
 
 exports.updateCard = async (req, res) => {
+  // Fetch the specific card by ID
   const card = await Card.findById(req.params.id);
+  // If the card's user ID does not match the user ID requested
   if (!card.user.equals(req.userId)) {
+    // Return forbidden
     return res.sendStatus(403);
   }
   const newValues = {
@@ -51,22 +64,26 @@ exports.updateCard = async (req, res) => {
     backText: req.body.backText,
     active: req.body.active,
   };
-
+  // Update the card with the new values
   card.set(newValues);
   await card.save();
 
   res.status(200).json(card);
 };
 
+// Updates card status to inactive or active
 exports.updateCardStatus = async (req, res) => {
+  // Fetch the specific card by ID
   const card = await Card.findById(req.params.id);
+  // If the card's user ID does not match the user ID requested
   if (!card.user.equals(req.userId)) {
+    // Return forbidden
     return res.sendStatus(403);
   }
   const newValue = {
     active: req.body.active,
   };
-
+  // Update the card's status
   card.set(newValue);
   await card.save();
 
